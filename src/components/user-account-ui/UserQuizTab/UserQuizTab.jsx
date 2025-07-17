6adc3ab3b926f6349b91d153066287fa98f42d9b
Maxessien <essienmax484@gmail.com>
2025-07-17 11:27:10 +0100

    fixed contrast in the light theme

--- src/components/user-account-ui/UserQuizTab/UserQuizTab.jsx
+++ src/components/user-account-ui/UserQuizTab/UserQuizTab.jsx
@@ -7,20 +7,26 @@ import { Link } from "react-router-dom";
 import AvailableQuizList from "../../layout-components/AvailableQuizList";
 // eslint-disable-next-line no-unused-vars
 import { AnimatePresence, motion } from "framer-motion";
+import useDarkMode from "../../stores-component/DarkLightThemeStore";
 // import useDarkMode from "../../stores-component/DarkLightThemeStore";
 // import { useQuery } from "@tanstack/react-query";
 
 function UserQuizTab() {
   const { allData } = useQuizData();
+  const { isDarkMode, setIsDarkMode } = useDarkMode();
   const [filterMenu, setFilterMenu] = useState(false);
   const courses = allData.map(({ course_code }) => {
     return course_code;
   });
-  // const [filteredCourses, setFilteredCourses] = useState([]);
+
+  //setting the theme of this component dynamically
+  useEffect(() => {
+    const theme = window.matchMedia("(prefers-color-scheme: dark)");
+    setIsDarkMode(theme);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isDarkMode]);
 
   const handleSelectedCourse = (state, action) => {
-    // console.log("stst", state);
-    // console.log("actt", action.type);
     if (action.type === "all") {
       return [...courses];
     } else if (!state.includes(action.type)) {
@@ -30,11 +36,13 @@ function UserQuizTab() {
     }
   };
 
+  //setting the selected categories to courses when it has been successfully fetched and preventing 
+  // infinite loop by making it run only when selected category is empty
   useEffect(() => {
     if (courses.length > 0 && selectedCategory.length < 1) {
       dispatch({ type: "all" });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [courses]);
 
   const [selectedCategory, dispatch] = useReducer(handleSelectedCourse, []);
@@ -58,7 +66,13 @@ function UserQuizTab() {
 
             <button
               className="filter-btn"
-              style={{background: filterMenu ? 'var(--blue-border)' : 'none'}}
+              style={{
+                background: filterMenu
+                  ? isDarkMode
+                    ? "var(--blue-border)"
+                    : "rgba(106, 150, 156, 1)"
+                  : "none",
+              }}
               onClick={() => setFilterMenu(!filterMenu)}
             >
               <FaFilter /> Filter
@@ -66,79 +80,87 @@ function UserQuizTab() {
           </div>
 
           <AnimatePresence mode="wait">
-          {filterMenu && (
-            <motion.div
-            variants={{
-              hidden: {
-                scaleY: 0,
-                transformOrigin: 'top'
-              },
-              visible: {
-                scaleY: 1,
-                transformOrigin: 'top'
-              }
-            }}
-            initial='hidden'
-            animate='visible'
-            exit='hidden'
-            transition={{
-              duration: 0.5
-            }}
-             className="filter-wrapper">
-              <div className="category">
-                <h3>By course</h3>
-                <ul className="all-courses">
-                  {allData.map(({ course_code }, index) => {
-                    console.log(index, course_code);
-                    return (
-                      <li
-                        style={{
-                          background: selectedCategory.includes(course_code)
-                            ? "var(--blue-border)"
-                            : "none",
-                        }}
-                        key={index}
-                        onClick={() => dispatch({ type: course_code })}
-                      >
-                        {course_code}
-                      </li>
-                    );
-                  })}
-                </ul>
-                <button onClick={() => dispatch({ type: "all" })}>Reset</button>
-              </div>
+            {filterMenu && (
+              <motion.div
+                variants={{
+                  hidden: {
+                    scaleY: 0,
+                    transformOrigin: "top",
+                  },
+                  visible: {
+                    scaleY: 1,
+                    transformOrigin: "top",
+                  },
+                }}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                transition={{
+                  duration: 0.5,
+                }}
+                className="filter-wrapper"
+              >
+                <div className="category">
+                  <h3>By course</h3>
+                  <ul className="all-courses">
+                    {allData.map(({ course_code }, index) => {
+                      console.log(index, course_code);
+                      return (
+                        <li
+                          style={{
+                            background: selectedCategory.includes(course_code)
+                              ? isDarkMode
+                                ? "var(--blue-border)"
+                                : "rgba(106, 150, 156, 1)"
+                              : "none",
+                          }}
+                          key={index}
+                          onClick={() => dispatch({ type: course_code })}
+                        >
+                          {course_code}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <button onClick={() => dispatch({ type: "all" })}>
+                    Reset
+                  </button>
+                </div>
 
-              <div className="completed-quiz-filter">
-                <input type="radio" id="attempted" name="completed-filter" />
-                <label htmlFor="attempted">Attempted</label>
+                <div className="completed-quiz-filter">
+                  <input type="radio" id="attempted" name="completed-filter" />
+                  <label htmlFor="attempted">Attempted</label>
 
-                <input
-                  type="radio"
-                  id="not-attempted"
-                  name="completed-filter"
-                />
-                <label htmlFor="not-attempted">Not Attempted</label>
-                <input type="radio" id="all" name="completed-filter" />
-                <label htmlFor="all">All</label>
-              </div>
-            </motion.div>
-          )}
+                  <input
+                    type="radio"
+                    id="not-attempted"
+                    name="completed-filter"
+                  />
+                  <label htmlFor="not-attempted">Not Attempted</label>
+                  <input type="radio" id="all" name="completed-filter" />
+                  <label htmlFor="all">All</label>
+                </div>
+              </motion.div>
+            )}
           </AnimatePresence>
         </header>
 
         {selectedCategory.length > 0 && (
           <>
             <section>
-              <ul className="user-available-quiz-list">
+              <ul className="available-quiz-list">
                 {allData.map(({ title, id, course_code }, index) => {
                   return (
                     <>
                       {selectedCategory.includes(course_code) ? (
-                        <Link style={{width: '100%'}} to={`/quiz/${id[index]}`}>
-                        <li key={id} className="user-quiz-link">
-                          {title}
-                        </li>
-                          </Link>
+                        <Link
+                          style={{ width: "100%" }}
+                          to={`/quiz/${id[index]}`}
+                        >
+                          <li key={id} className="quiz-link">
+                            {title}
+                          </li>
+                        </Link>
                       ) : null}
                     </>
                   );
